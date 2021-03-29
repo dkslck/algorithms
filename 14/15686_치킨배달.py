31e7faf7e71b8a8e8d896fff35016876a67385b5
dkslck <77281452+dkslck@users.noreply.github.com>
2021-03-29 20:00:03 +0900

    Create 15686_치킨배달.py

---
+++ 14/15686_치킨배달.py
@@ -0,0 +1,147 @@
+# CP template Version 1.006
+import os
+import sys
+import itertools
+import collections
+import string
+# not for python < 3.9
+# from functools import cmp_to_key, reduce, partial, cache
+from functools import cmp_to_key, reduce, partial
+from itertools import product
+from collections import deque, Counter, defaultdict as dd
+from math import log, log2, ceil, floor, gcd, sqrt
+import math
+from heapq import heappush, heappop
+from bisect import bisect_left as bl, bisect_right as br
+DEBUG = False
+
+
+def main(f=None):
+    init(f)
+    # sys.setrecursionlimit(10**9)
+    # ####################################
+    # ######## INPUT AREA BEGIN ##########
+
+    N, M = map(int, input().split())
+    mat = [list(map(int, input().split())) for _ in range(N)]
+
+    # ######## INPUT AREA END ############
+    # ####################################
+
+    global houses, chickens
+    houses = []
+    chickens = []
+    for i in range(N):
+        for j in range(N):
+            if mat[i][j] == 1:
+                houses.append((i, j))
+            if mat[i][j] == 2:
+                chickens.append((i, j))
+
+
+    min_ = 10**19
+    for chickenPicks in itertools.combinations(chickens, M):
+        min_ = min(min_, calculateMin(chickenPicks))
+
+    print(min_)
+
+
+def calculateMin(picks):
+    totalDistance = 0
+
+    for house in houses:
+        distance = 10 ** 9
+        for chicken in picks:
+            distance = min(distance, calculateDistance(house, chicken))
+        totalDistance += distance
+    return totalDistance
+
+
+def calculateDistance(p1, p2):
+    return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
+
+
+
+
+
+
+
+
+# #############################################################################
+# #############################################################################
+# ############################## TEMPLATE AREA ################################
+# #############################################################################
+# #############################################################################
+
+enu = enumerate
+
+
+def argmax(arr):
+    return max(enumerate(arr), key=lambda x: x[1])
+
+
+def argmin(arr):
+    return min(enumerate(arr), key=lambda x: x[1])
+
+
+def For(*args):
+    return itertools.product(*map(range, args))
+
+
+def copy2d(mat):
+    return [row[:] for row in mat]
+
+
+def Mat(h, w, default=None):
+    return [[default for _ in range(w)] for _ in range(h)]
+
+
+def nDim(*args, default=None):
+    if len(args) == 1:
+        return [default for _ in range(args[0])]
+    else:
+        return [nDim(*args[1:], default=default) for _ in range(args[0])]
+
+
+def setStdin(f):
+    global DEBUG, input
+    DEBUG = True
+    sys.stdin = open(f)
+    input = sys.stdin.readline
+
+
+def init(f=None):
+    global input
+    input = sys.stdin.readline  # by default
+    if os.path.exists("o"):
+        sys.stdout = open("o", "w")
+    if f is not None:
+        setStdin(f)
+    else:
+        if len(sys.argv) == 1:
+            if os.path.isfile("in/i"):
+                setStdin("in/i")
+            elif os.path.isfile("i"):
+                setStdin("i")
+        elif len(sys.argv) == 2:
+            setStdin(sys.argv[1])
+        else:
+            assert False, "Too many sys.argv: %d" % len(sys.argv)
+
+
+def dprint(*args):
+    if DEBUG:
+        print(*args)
+
+
+def pfast(*args, end="\n", sep=' '):
+    sys.stdout.write(sep.join(map(str, args)) + end)
+
+
+def parr(arr):
+    for i in arr:
+        print(i)
+
+
+if __name__ == "__main__":
+    main()
